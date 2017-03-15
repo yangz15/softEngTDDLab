f24453667d2026c5ede8e297e969d5d65b7d5f41
Travis Palmer <hofnug@gmail.com>
2017-03-15 15:07:02 -0400

    Basic tests in, continued edits - partially runnable

--- recruiter.js
+++ recruiter.js
@@ -1,31 +1,8 @@
 'use strict';
-var exports = module.exports = {};
 
 // Object containing starting wages for various 4 year degrees
 var degreeSWage = require('degreeSWage.json');
-
-// getValueFromWageAndExp( wage, full years of experiance );
-function getValueFromWageAndExp(wage, fullYearsExp) {
-	if (fullYearsExp !== Math.floor(fullYearsExp)) {
-		console.error("getValueFromWageAndExp was given a partial year of experiance")
-		return false;
-	}
-	var modYears;
-	if (fullYearsExp >= 5) {
-		modYears = 5;
-	} else {
-		modYears = fullYearsExp;
-	}
-	modYears = 1 + (modYears - 1) * 0.2;
-	return modYears * wage;
-}
-
-// sortInternObjects( Array of hireables with "metric" as a property )
-function sortInternObjects(internArr) {
-	internArr.sort(function (a, b) {
-  	return b.metric - a.metric;
-	});
-}
+var util = require('util.js');
 
 //TODO: You need to write this function AND utilize it.
 // bracketFromGPA(decimal GPA);
@@ -35,7 +12,7 @@ function bracketFromGPA(gpa) {
 }
 
 // recruiter( Array of hireables )
-exports.recruiter = function(internArr) {
+function recruiter(internArr) {
 
 	// Below is just to help show the syntax you need,
 	// you'll need to process all of the hireables like this one and sort
@@ -51,17 +28,17 @@ exports.recruiter = function(internArr) {
 	iwage = degreeSWage[idegr];
 
 	// You should use these functions at some point
-	ivalue = getValueFromWageAndExp( /*wage, full years of experiance*/ );
+	ivalue = util.getValueFromWageAndExp( /*wage, full years of experiance*/ );
 	ibracket = bracketFromGPA ( /*decimal GPA*/ );
 
 	// Hmm... this doesn't seem to follow the spec - fix it
 	imetric = ivalue + ibracket;
 
-	// We really want to add our sorting number "metric" to objects (really is this easy)
+	// We really want to add our sorting number "metric" to objects (it really is this easy)
 	internArr[index].metric = imetric;
 
 	// and then sort them all
-	sortInternObjects( /*Array of hireables with "metric" as a property*/ );
+	util.sortInternObjects( /*Array of hireables with "metric" as a property*/ );
 
 
 	// Output 
@@ -71,5 +48,10 @@ exports.recruiter = function(internArr) {
 	// and people earlier in the array have equal or greater values for "metric" than
 	// people further down.
 
-	return arrayOfHireableInterns;
+	return internArr;
+};
+
+module.exports = {
+	recruiter: recruiter,
+	bracketFromGPA: bracketFromGPA
 };
--- test/index.js
+++ test/index.js
@@ -5,4 +5,58 @@ var test = require('tape');
 var potentialHires = require('../input/groupOne.json');
 var interns = potentialHires.interns;
 
-// Tests go here
+var recruiter = require('recruiter.js');
+var util = require('util.js');
+
+test('util.getValueFromWageAndExp', function(t) {
+  t.ok(util.getValueFromWageAndExp(31, 1) > util.getValueFromWageAndExp(30, 1), 'factors in wage');
+
+  if (util.getValueFromWageAndExp(30, 1) > util.getValueFromWageAndExp(30, 0)) {
+  	t.pass('factors in experiance');
+  } else {
+  	t.fail('does not factor in experiance');
+  }
+
+  t.throws(function() { util.getValueFromWageAndExp(34, 1.3); },
+  	/was given a partial year/,
+  	'errors on partial years');
+
+  t.end();
+});
+
+test('util.sortInternObjects', function(t) {
+	inputArr = [interns[0], interns[1], interns[2], interns[3]];
+	inputArr[0].metric = 3;
+	inputArr[1].metric = 1;
+	inputArr[2].metric = 2;
+	inputArr[3].metric = 0;
+
+	expectedArr = [
+		inputArr[0],
+		inputArr[2],
+		inputArr[1],
+		inputArr[3]
+	];
+
+  t.deepEqual(util.sortInternObjects(inputArr), expectedArr, 'bascially sorts by metric');
+
+  inputArr[0].metric = 0;
+
+  expectedArr = [
+		inputArr[2],
+		inputArr[1],
+		inputArr[0],
+		inputArr[3]
+	];
+
+	t.deepEqual(util.sortInternObjects(inputArr), expectedArr, 'preserves order of same-metric objects');
+
+  t.end();
+});
+
+test('@define-placeholder works with several added selectors', function(t) {
+  compareFixtures(t, 'several-additions');
+  t.end();
+});
+
+// Your tests go here  (methods reference: https://www.npmjs.com/package/tape#testname-opts-cb )
